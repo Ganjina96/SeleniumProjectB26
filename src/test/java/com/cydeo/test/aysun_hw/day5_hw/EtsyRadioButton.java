d3b8cffea9018c25549423f2051ba8ab9e4701f2
Ganjina96 <gnagzibe@gmail.com>
2022-06-02 10:59:28 -0400

    latest commits

---
+++ src/test/java/com/cydeo/test/aysun_hw/day5_hw/EtsyRadioButton.java
@@ -0,0 +1,57 @@
+package com.cydeo.test.aysun_hw.day5_hw;
+
+import com.cydeo.test.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class EtsyRadioButton {
+
+    @Test
+    public void etsyTest(){
+//        TC: Etsy checkbox and radio button
+
+//        1. Go to https://www.etsy.com
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.etsy.com");
+
+//        2. Search for “wooden spoon”
+        WebElement woodenSpoon = driver.findElement(By.xpath("//input[contains(@id,'global-enhancements')]"));
+        woodenSpoon.sendKeys("wooden spoon" + Keys.ENTER);
+
+//        3. Click on filters
+        WebElement filterBtn = driver.findElement(By.xpath("//span[.='All Filters']"));
+        filterBtn.click();
+
+//        4. Select free shipping, on sale
+        WebElement freeShip = driver.findElement(By.xpath(("//label[contains(.,'FREE shipping')]")));
+        WebElement onSale = driver.findElement(By.xpath("//label[contains(.,'On sale')]"));
+        freeShip.click();
+        onSale.click();
+
+//        5. Select under $25 Click on apply filters
+        WebElement under25 = driver.findElement(By.xpath("//label[contains(.,'Under $25')]"));
+        under25.click();
+
+//        6. Print count of results
+        WebElement applyBtn = driver.findElement(By.xpath("//button[@aria-label='Apply']"));
+        applyBtn.click();
+        // explicit wait - to wait for the  visibility of "Remove At Most $25 Filter" element
+
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@aria-label='Remove At Most $25 Filter']")));
+
+        WebElement results = driver.findElement(By.xpath("//span[@class='wt-display-inline-flex-sm']/span[1]"));
+        System.out.println("results.getText() = " + results.getText());
+
+        driver.quit();
+    }
+}
--- src/test/java/com/cydeo/test/day6_StaleElement_TestNG/TestNG_Intro.java
+++ src/test/java/com/cydeo/test/day6_StaleElement_TestNG/TestNG_Intro.java
@@ -1,5 +1,53 @@
 package com.cydeo.test.day6_StaleElement_TestNG;
 
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+
 public class TestNG_Intro {
+
+    @BeforeClass
+    public void setUp(){
+        System.out.println("Setting up...");
+    }
+
+    @AfterClass
+    public void tearDown(){
+        System.out.println("After class is done");
+    }
+
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("Setting up method");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("After method is done");
+    }
+
+    @Test (priority = 2)
+    public void test1(){
+        System.out.println("Test 1 is running...");
+        String actual = "apple";
+        String expected = "apple";
+
+        // assertEquals(actual,expected, "Test failed"), message is optional and will only appear if it fails
+        Assert.assertEquals(actual,expected, "Test failed");
+
+    }
+
+//    public void test2(){
+//        System.out.println("Test 2 is running..."); // won't run because there is no @Test annotation, it is not runnable
+//    }
+
+    @Test (priority = 1)
+    public void test2(){
+        System.out.println("Test 2 is running...");
+
+        Assert.assertEquals("orange", "orange", "failed");
+    }
+
+
 
 }
