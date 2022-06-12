007f9dcc1e69e8d858956027f8a094c36aef4283
Ganjina96 <gnagzibe@gmail.com>
2022-06-12 12:02:02 -0400

    latest commits

--- src/test/java/com/cydeo/test/day11_webtables_utilities/T1_Multiple_Windows.java
+++ src/test/java/com/cydeo/test/day11_webtables_utilities/T1_Multiple_Windows.java
@@ -1,6 +1,7 @@
 package com.cydeo.test.day11_webtables_utilities;
 
 import com.cydeo.test.base.TestBase;
+import com.cydeo.test.utilities.BrowserUtils;
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -22,21 +23,22 @@ public class T1_Multiple_Windows extends TestBase {
         ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
 
 //        4. Create a logic to switch to the tab where Etsy.com is open
-        Set<String> allWindowHandles = driver.getWindowHandles();
-
-        for (String eachWindow : allWindowHandles) {
-            driver.switchTo().window(eachWindow);
-            System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
-            if (driver.getCurrentUrl().contains("etsy")){
-                break;
-            }
-        }
-
-//        5. Assert:Title contains “Etsy”
-        String actualTitle = driver.getTitle();
-        String expectedTitle = "Etsy";
-        Assert.assertTrue(actualTitle.contains(expectedTitle));
-
+//        Set<String> allWindowHandles = driver.getWindowHandles();
+//
+//        for (String eachWindow : allWindowHandles) {
+//            driver.switchTo().window(eachWindow);
+//            System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
+//            if (driver.getCurrentUrl().contains("etsy")){
+//                break;
+//            }
+//        }
+//
+////        5. Assert:Title contains “Etsy”
+//        String actualTitle = driver.getTitle();
+//        String expectedTitle = "Etsy";
+//        Assert.assertTrue(actualTitle.contains(expectedTitle));
+//
+        BrowserUtils.switchWindowAndVerify(driver, "etsy","Etsy");
 
 
 
